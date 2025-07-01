15c71419a8fe38b9a04624166e797140b2649d85
Sergio Arbesu <sergioarbesu2004@gmail.com>
2025-07-01 16:58:51 -0700

    Deleted Setup project and created uninstall arg for custom actions

--- ChampSelectHelperApp/Program.cs
+++ ChampSelectHelperApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading;
+using System.Diagnostics;
 
 namespace ChampSelectHelper
 {
@@ -9,6 +10,7 @@ namespace ChampSelectHelper
         public static readonly string APP_NAME = "Champ Select Helper";
         public static readonly string APP_VERSION = "1.0";
 
+        public static readonly string UNINSTALL_FLAG = "--uninstall";
         public static readonly string WINDOWS_STARTUP_FLAG = "--systemstartup";
         public static bool LAUNCHED_AT_STARTUP;
 
@@ -19,6 +21,22 @@ namespace ChampSelectHelper
         [STAThread]
         static void Main(string[] args)
         {
+            if (args.Contains(UNINSTALL_FLAG))
+            {
+                if (FileHandler.LaunchesAtStartup()) FileHandler.ToggleLaunchAtStartup();
+
+                Process current = Process.GetCurrentProcess();
+
+                foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+                {
+                    if (process.Id != current.Id)
+                    {
+                        process.Kill();
+                    }
+                }
+                return;
+            }
+
             if (!mutex.WaitOne(TimeSpan.Zero, false)) return;
 
             LAUNCHED_AT_STARTUP = args.Contains(WINDOWS_STARTUP_FLAG);
